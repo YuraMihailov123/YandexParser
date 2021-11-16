9a9642386c1cdb6cd2a14df32bb72d2ef3cf27b6
YuraMihailov <recon.97@mail.ru>
2021-11-16 09:22:11 +0300

    UPD: added

---
+++ Pages/writeToDatabase.php
@@ -0,0 +1,26 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "Test1234";
+$dbname = "MarketDB";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} //- коннектимся к бд с нашими данными сервера, юзера, пароля и бд
+
+echo 'Connected successfully';
+
+$q = $_REQUEST["q"]; // - получаем запрос, который отправили с локалхоста парсека
+
+if ($q !== "") { // - если не пустой то выполняем запрос
+	if ($conn->query($q) === TRUE) {
+    		echo "New record created successfully";
+	} else {
+    		echo "Error: " . $q . "<br>" . $conn->error;
+	}
+}
+
+$conn->close();
+?>
