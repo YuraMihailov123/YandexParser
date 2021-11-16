ab15f534486b15898d866dfc8bf804e390508c0f
YuraMihailov <recon.97@mail.ru>
2021-11-16 12:43:54 +0300

    Update writeToDatabase.php

--- Pages/writeToDatabase.php
+++ Pages/writeToDatabase.php
@@ -5,9 +5,9 @@ $username = "root";
 $password = "Test1234";
 $dbname = "MarketDB";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 } //- коннектимся к бд с нашими данными сервера, юзера, пароля и бд
 
 echo 'Connected successfully';
